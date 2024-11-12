a0d7158f184e0df4fc9b031708f52c75ca605fde
meta100py <meta100py@gmail.com>
2024-11-12 18:39:01 -0300

    Added links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,15 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was created by <a href="https://github.com/meta100py" target="_blank" rel="noopener noreferrer">
+      Valen
+      </a> and is{" "}
+      <a href="https://github.com/meta100py/weather-react-app-2" target="_blank" rel="noopener noreferrer">
+      open-sourced on Github
+      </a>
+     </footer> 
     </div>
   );
 }
