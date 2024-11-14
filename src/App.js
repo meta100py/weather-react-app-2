74fbc750fddb7bc6b4129631d1830bc4112e031e
meta100py <meta100py@gmail.com>
2024-11-14 15:35:01 -0300

    Added the css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <Weather />
-      <footer>
-        This project was created by <a href="https://github.com/meta100py" target="_blank" rel="noopener noreferrer">
-      Valen
-      </a> and is{" "}
-      <a href="https://github.com/meta100py/weather-react-app-2" target="_blank" rel="noopener noreferrer">
-      open-sourced on Github
-      </a>
-     </footer> 
+        <Weather />
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://github.com/meta100py"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Valen
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/meta100py/weather-react-app-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
